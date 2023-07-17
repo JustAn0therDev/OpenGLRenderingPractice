05e4fccb8892a0e744cf84afb1c6a498df7b687d
Ruan Montelo <ruanmontelo@gmail.com>
2023-07-17 17:13:34 -0300

    Drawing a square and changing the color of the triangles by changing the shader program

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,37 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+bool wireframeToggle = false;
+bool shaderToggle = false;
+
+void wireframe_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+	if (key == GLFW_KEY_F && action == GLFW_RELEASE) {
+		if (wireframeToggle) {
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+			wireframeToggle = false;
+		}
+		else {
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+			wireframeToggle = true;
+		}
+	}
+	else if (key == GLFW_KEY_S && action == GLFW_RELEASE) {
+		if (shaderToggle) {
+			shaderToggle = false;
+		}
+		else {
+			shaderToggle = true;
+		}
+	}
+	else if (key == GLFW_KEY_ESCAPE) {
+		exit(1);
+	}
+}
+
+// Window settings
+constexpr int WIDTH = 1024;
+constexpr int HEIGHT = 768;
+
 // Vertex Shader
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
@@ -9,12 +40,21 @@ const char* vertexShaderSource = "#version 330 core\n"
 "{\n"
 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 "}\0";
+
 //Fragment Shader
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
 "   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"}\n\0";
+
+//Second Fragment Shader
+const char* secondFragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
 "}\n\0";
 
 int main(void) {
@@ -25,15 +65,21 @@ int main(void) {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLfloat vertices[] =
+	float vertices[] =
 	{
-		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f
+		 0.5f,  0.5f, 0.0f,  // top right
+		 0.5f, -0.5f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  // bottom left
+		-0.5f,  0.5f, 0.0f   // top left 
+	};
+
+	unsigned int indices[] = {  // note that we start from 0!
+		0, 1, 3,   // first triangle
+		1, 2, 3    // second triangle
 	};
 
 	// Create window
-	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGLRenderingPractice", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "OpenGLRenderingPractice", NULL, NULL);
 
 	// Error handling
 	if (window == nullptr) {
@@ -41,14 +87,21 @@ int main(void) {
 		return 1;
 	}
 
-	// Make context current (not really sure what this means yet...)
+	// Get the primary monitor's current resolution
+	const GLFWvidmode* vidMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+	// And set the window position to always be centered on the screen.
+	glfwSetWindowPos(window, (vidMode->width / 2) - WIDTH / 2, (vidMode->height / 2) - HEIGHT / 2);
+
+	// Make context current. Every that happens in OpenGL will affect this window (or this "context").
 	glfwMakeContextCurrent(window);
 
 	// Load glad so it configures OpenGL
 	gladLoadGL();
 
-	// Set the viewport (limits to where to draw the content)
-	glViewport(0, 0, 800, 600);
+	// Set the viewport (limits to where to draw the content. The positions in OpenGL are all normalized,
+	// so it needs to know the screen size beforehand.)
+	glViewport(0, 0, WIDTH, HEIGHT);
 
 	// Create the vertex shader, set the source and compile it
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -60,27 +113,39 @@ int main(void) {
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
 	glCompileShader(fragmentShader);
 
+	// Now a second fragment shader, which sets a different color
+	GLuint secondFragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(secondFragmentShader, 1, &secondFragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
 	// Now create the shader program
-	GLuint shaderProgram = glCreateProgram();
+	GLuint firstTriangleShaderProgram = glCreateProgram();
+	GLuint secondTriangleShaderProgram = glCreateProgram();
 
 	// Attach the previously created shaders
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
+	glAttachShader(firstTriangleShaderProgram, vertexShader);
+	glAttachShader(secondTriangleShaderProgram, vertexShader);
+
+	glAttachShader(firstTriangleShaderProgram, fragmentShader);
+	glAttachShader(secondTriangleShaderProgram, secondFragmentShader);
+
+	// Links the shaders to the shader program object
+	// to generate a working shader program.
+	glLinkProgram(firstTriangleShaderProgram);
+	glLinkProgram(secondTriangleShaderProgram);
 
 	// Then delete them, since the program has already been created,
 	// and we want to always free the resources that are not being used anymore.
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
 
 	// The vertex array and vertex buffer objects.
-	GLuint VAO, VBO;
+	GLuint VAO, VBO, EBO;
 
-	// Always generate the VAO before the VBO.
+	// Always generate the VAO before the VBO (and EBO apparently).
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
-	// Bind the created vertex array.
+	// Bind the created vertex array object.
 	glBindVertexArray(VAO);
 	
 	// Bind the buffer object
@@ -88,25 +153,23 @@ int main(void) {
 	// Set the buffer data
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	// Bind the buffer object
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	// Set the buffer data
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	// Call vertex attribute pointer
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	// Enable it
 	glEnableVertexAttribArray(0);
 
 	// Bind to 0
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(VBO, 0);
 	
 	// Same for array
 	glBindVertexArray(0);
 
-	// Clear the color.
-	glClearColor(0.0f, 0.0f, 255.0f, 1.0f);
-
-	// Set the buffer to be the back buffer.
-	glClear(GL_COLOR_BUFFER_BIT);
-
-	// And tell glfw to swap the buffers.
-	glfwSwapBuffers(window);
+	glfwSetKeyCallback(window, wireframe_callback);
 
 	// While loop so the window does not close.
 	while (!glfwWindowShouldClose(window)) {
@@ -116,19 +179,33 @@ int main(void) {
 		// Set the buffer to be the back buffer.
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glUseProgram(shaderProgram);
+		if (shaderToggle) {
+			glUseProgram(secondTriangleShaderProgram);
+		}
+		else {
+			glUseProgram(firstTriangleShaderProgram);
+		}
+		
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
+
 		// Make sure to poll events so the window is not frozen.
 		glfwPollEvents();
 	}
 
 	// Shader and vertex data cleanup.
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	glDeleteShader(secondFragmentShader);
+
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
-	glDeleteProgram(shaderProgram);
+	glDeleteBuffers(1, &EBO);
+	glDeleteProgram(firstTriangleShaderProgram);
+	glDeleteProgram(secondTriangleShaderProgram);
 
 	// Destroy the window when the program is about to exit.
 	glfwDestroyWindow(window);
